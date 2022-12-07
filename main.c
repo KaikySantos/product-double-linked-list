742766d087f06694b93613f11829c8bcdc424509
kaikySantos <info.kaiky@gmail.com>
2022-12-07 14:09:22 -0300

    feat: another linked list

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct List_{
+typedef struct List_ {
   struct List_ *prev;
   int value;
   struct List_ *next;
-}List;
+} List;
 
-List* makeNode(int value){
+List* makeNode(int value) {
   List *n = malloc(sizeof(List));
   n->prev = NULL;
   n->value = value;
@@ -15,27 +15,36 @@ List* makeNode(int value){
   return n;
 }
 
-List *makeNum(List *num, int value){
+List *makeNum(List *num, int value) {
   List *tmp = makeNode(value);
   tmp->next = num;
   num->prev = tmp;
   return tmp;
 }
 
-int main(void){
-  List *num, *tmp;
-  List *uni = makeNode(958);
-  
-  num = uni;
-  num = makeNum(num, 979);
-  num = makeNum(num, 41);
-  num = makeNum(num, 20);
+int main(void) {
+  List *num1, *tmp1, *num2, *tmp2;
 
-  tmp = num;
+  List *uni1 = makeNode(8);
+  num1 = uni1;
+  num1 = makeNum(num1, 9);
+  tmp1 = num1;
 
-  while(tmp){
-    printf("%03d \n", tmp->value);
-    tmp = tmp->next;
+  List *uni2 = makeNode(5);
+  num2 = uni2;
+  num2 = makeNum(num2, 4);
+  tmp2 = num2;
+
+  printf("One: \n");
+  while (tmp1) {
+    printf("%d \n", tmp1->value);
+    tmp1 = tmp1->next;
+  }
+
+  printf("Two: \n");
+  while (tmp2) {
+    printf("%d \n", tmp2->value);
+    tmp2 = tmp2->next;
   }
 
   printf("\n");
