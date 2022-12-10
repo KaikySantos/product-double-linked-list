0cada4a2badc0d95df4bd853f20489f55563fa95
kaikySantos <info.kaiky@gmail.com>
2022-12-10 18:02:33 -0300

    feat: separating string

--- main.c
+++ main.c
@@ -23,17 +23,47 @@ List *makeNum(List *num, int value) {
   return tmp;
 }
 
+char* reverseString(char vet[]){
+  int i, aux, fim, tam = strlen(vet);
+  char *novo = malloc((tam + 1) * sizeof(char));
+
+  fim = tam - 1; // índice do último caracter
+  for(i = 0; i < tam; i++){
+      novo[fim] = vet[i];
+      fim--;
+  }
+  novo[i] = '\0'; // finaliza a nova string
+  return novo;
+}
+
 int main(void) {
-  char num1[] = "154789";
+  char num1[] = "1123456789";
   char num2[] = "243678";
 
   printf("First number: %s\n", num1);
-  printf("Second number: %s\n", num2);
+  // printf("Second number: %s\n", num2);
 
-  char newNumber[] = "";
+  List *num, *tmp;
 
-  for (int i=0; i < strlen(num1); i++) {
-    newNumber = num1[i];
-    printf("%s \n", newNumber);
+  List *uni = makeNode(num1[strlen(num1) - 1]-'0');
+  num = uni;
+
+  char numberAux[4] = "";
+  for (int i=strlen(num1)-1; i >= 0; i--) {
+    if (strlen(numberAux) < 3) {
+      numberAux[strlen(numberAux)] = num1[i];
+    }
+
+    if (strlen(numberAux) == 3 || i == 0) {
+      printf("- %s \n", reverseString(numberAux));
+      memset(numberAux, 0, sizeof(numberAux));
+    }
   }
+
+  // tmp = num;
+  // while(tmp) {
+  //   printf("%d->", tmp->value);
+  //   tmp = tmp->next;
+  // }
+  // printf("\n");
 }
